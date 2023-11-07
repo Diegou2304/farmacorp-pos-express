ea6eee306e1be364853c60272ce6dfc3384ec35b
Diegou2304 <diegou2304-98@hotmail.com>
2023-11-07 16:07:31 -0400

    Initial Domain Classes

---
+++ FarmacorpPOS.Domain/Express/ExpressSale.cs
@@ -0,0 +1,15 @@
+﻿namespace FarmacorpPOS.Domain.Express
+{
+    public class ExpressSale
+    {
+        public int IdExpressSale { get; set; }
+        public DateTime SaleDate { get; set; }
+        public string ClientFullName { get; set; } = string.Empty;
+        public string ProductName { get; set; }
+        public string UniqueProductCode { get; set; }
+        public double Price { get; set; }
+        public string Discount { get; set; }
+        public string TotalPrice { get; set; }
+
+    }
+}
---
+++ FarmacorpPOS.Domain/Express/ProductType.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace FarmacorpPOS.Domain.Express
+{
+    public class ProductType
+    {
+        public int IdProductType { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
