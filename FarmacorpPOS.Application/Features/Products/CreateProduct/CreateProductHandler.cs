80fc705fbf288af37d47332ca2b1aea73c35edd2
Diegou2304 <diegou2304-98@hotmail.com>
2023-11-08 15:54:30 -0400

    Completado Unit Of Work para la creación de productos

--- FarmacorpPOS.Application/Features/Products/CreateProduct/CreateProductHandler.cs
+++ FarmacorpPOS.Application/Features/Products/CreateProduct/CreateProductHandler.cs
@@ -1,7 +1,7 @@
 ﻿using FarmacorpPOS.Application.Features.Sales.Utils;
 using FarmacorpPOS.Domain.ERP;
 using FarmacorpPOS.Domain.Express;
-using FarmacorpPOS.Infrastructure.Repositories.Contracts;
+using FarmacorpPOS.Infrastructure.Repositories;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -10,19 +10,19 @@ namespace FarmacorpPOS.Application.Features.Products.CreateProduct
 {
     public class CreateProductHandler : IRequestHandler<CreateProductCommand, IActionResult>
     {
-        private readonly IProductRepository _productRepository;
         private readonly IOptions<SaleStrategyConfig> _saleStrategyConfig;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public CreateProductHandler(IProductRepository productRepository, IOptions<SaleStrategyConfig> saleStrategyConfig)
+        public CreateProductHandler(IOptions<SaleStrategyConfig> saleStrategyConfig, IUnitOfWork unitOfWork)
         {
-            _productRepository = productRepository;
             _saleStrategyConfig = saleStrategyConfig;
+            _unitOfWork = unitOfWork;
         }
         public async Task<IActionResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
             var priceMultiplier = 1.5;
             if (_saleStrategyConfig.Value.SaleMode is "NewSale") priceMultiplier = 1.8;
-            var productType = await _productRepository.GetProductTypeById(request.ProductTypeId);
+            var productType = await _unitOfWork.ProductRepository.GetProductTypeById(request.ProductTypeId);
 
             if (productType is null) return new BadRequestObjectResult(new { Message = "El tipo de producto no existe, por favor seleccione otro" });
 
@@ -32,13 +32,16 @@ namespace FarmacorpPOS.Application.Features.Products.CreateProduct
                 request.Observations,
                 request.ProductTypeId,
                 newErpProduct,
-                request.ExpirationDate, 
+                request.ExpirationDate,
                 priceMultiplier);
 
-            await _productRepository.AddProductAsync(newProduct);
+            await _unitOfWork.ProductRepository.AddProductAsync(newProduct);
+            var result = await _unitOfWork.Complete();
+            if (result > 0) return new OkObjectResult(new { newProduct.ProductId });
 
-            return new OkObjectResult(new { newProduct.ProductId });
-            
+            return new BadRequestResult();
+
+
         }
     }
 }
